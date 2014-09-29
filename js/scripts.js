2b517e8ad901ce8c837931dfdff3eae2100ba3aa
webmikez <webmikez@gmail.com>
2014-09-29 14:21:40 +0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+new WOW().init();
+
+window.onload = function(){
+	$('#sites-slider').lemmonSlider({
+		'infinite' : false
+	});
+}
+
+$(function () {
+	$('#menu').stickyNavbar({
+		animDuration: 700,
+        cssAnimation: "fade"
+	});
+	
+	
+	$(".slider li").mouseover(function() {
+	 // $(".slider li .text").fadeIn(500);
+	});
+	$(".slider li").mouseleave(function() {
+	 // $(".slider li .text").fadeOut(500);
+	});
+	
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 0) {
+			$('#scroller').fadeIn();
+		} else {
+			$('#scroller').fadeOut();
+		}
+	});
+	
+	$('#totop').click(function () {
+		$('html, body').animate({scrollTop: 0}, 500);
+		return false;
+	});
+	
+});
